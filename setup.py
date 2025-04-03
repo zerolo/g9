a670a458b115a1a79b53840dbf4ddabdb4333563
zerolo <ze.p.rolo@gmail.com>
2025-04-03 17:10:48 +0100

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="g9_api_client",
-    version="0.0.1",
+    version="0.0.2",
     python_requires='>=3',
     package_dir={'': '.'},
     packages=find_namespace_packages(where='.'),
