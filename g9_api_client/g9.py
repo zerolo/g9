98bdd541877f2ddc46a4d02f351eee5fdf47c8f7
zerolo <ze.p.rolo@gmail.com>
2025-04-04 14:18:27 +0100

    aiohttp implementation

--- g9_api_client/g9.py
+++ g9_api_client/g9.py
@@ -1,32 +1,48 @@
-import httpx
-
+import aiohttp
+import logging
 from .constants import LOGIN_URL, INVOICES_URL, CONTRACTS_URL, ELECTRICITY_CONSUMPTION_URL, GAS_CONSUMPTION_URL
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.DEBUG)
 
 
 class G9:
-    def __init__(self, username, password):
+    def __init__(self, session: aiohttp.ClientSession, username, password):
         self.token = None
-        self.username = username
-        self.password = password
+        self._username = username
+        self._password = password
+        self._session = session
 
-    def login(self):
+    async def __api_request(self, url: str, method="get", data=None, params=None, headers=None):
+        async with getattr(self._session, method)(url, headers=headers or self._get_headers(), json=data,
+                                                  params=params, ssl=False) as response:
+            try:
+                if response.status == 200 and response.content_type == 'application/json':
+                    json_response = await response.json()
+                    return json_response
+                else:
+                    raise Exception(f"HTTP Request Error: %s", str(response.status) + " " + str(response.content_type))
+            except Exception as err:
+                _LOGGER.error(f"API request error: %s", err)
+                return None
+
+    async def login(self):
         headers = {
             'Content-Type': 'application/json'
         }
         data = {
-            'username': self.username,
-            'password': self.password
+            'username': self._username,
+            'password': self._password
         }
         print("Logging in...", LOGIN_URL)
-        print("Logging in with username:", self.username)
-        response = httpx.post(LOGIN_URL, json=data, headers=headers)
-        if response.status_code == 200:
-            result = response.json()
-            data = result.get('data')
-            if data:
-                self.token = data.get('token')
-                if self.token:
-                    return True
+        print("Logging in with username:", self._username)
+
+        result = await self.__api_request(LOGIN_URL, method="post", data=data, headers=headers)
+        data = result.get('data')
+        if data:
+            self.token = data.get('token')
+            if self.token:
+                return True
         return False
 
     def _get_headers(self):
@@ -35,48 +51,51 @@ class G9:
             "Cookie": f"BEARER={self.token}"
         }
 
-    def get_contracts(self):
-        response = httpx.get(CONTRACTS_URL, headers=self._get_headers())
-        if response.status_code == 200:
-            result = response.json()
-            contracts = result.get('contracts')
-            if contracts:
-                return contracts
+    async def get_contracts(self):
+        if not self.token:
+            _LOGGER.error("Not logged in - token not found")
+            raise Exception("Not logged in")
+        result = await self.__api_request(CONTRACTS_URL)
+        contracts = result.get('contracts')
+        if contracts:
+            return contracts
         return None
 
-    def get_last_invoice(self, contract_id):
-        if self.token:
-            params = {
-                "contractId": contract_id
-            }
-            response = httpx.get(INVOICES_URL, params=params, headers=self._get_headers())
-            if response.status_code == 200:
-                result = response.json()
-                invoice = result.get('invoice')
-                if invoice:
-                    return invoice
-        return None
-
-    def get_last_electricity_consumption(self, contract_id):
+    async def get_last_invoice(self, contract_id):
+        if not self.token:
+            _LOGGER.error("Not logged in - token not found")
+            raise Exception("Not logged in")
         params = {
             "contractId": contract_id
         }
-        response = httpx.get(ELECTRICITY_CONSUMPTION_URL, params=params, headers=self._get_headers())
-        if response.status_code == 200:
-            result = response.json()
-            electricity = result.get('electricity_graph')
-            if electricity:
-                return electricity
+        result = await self.__api_request(INVOICES_URL, params=params)
+        invoice = result.get('invoice')
+        if invoice:
+            return invoice
         return None
 
-    def get_last_gas_consumption(self, contract_id):
+    async def get_last_electricity_consumption(self, contract_id):
+        if not self.token:
+            _LOGGER.error("Not logged in - token not found")
+            raise Exception("Not logged in")
         params = {
             "contractId": contract_id
         }
-        response = httpx.get(GAS_CONSUMPTION_URL, params=params, headers=self._get_headers())
-        if response.status_code == 200:
-            result = response.json()
-            gas = result.get('gas_graph')
-            if gas:
-                return gas
+        result = await self.__api_request(ELECTRICITY_CONSUMPTION_URL, params=params)
+        electricity = result.get('electricity_graph')
+        if electricity:
+            return electricity
+        return None
+
+    async def get_last_gas_consumption(self, contract_id):
+        if not self.token:
+            _LOGGER.error("Not logged in - token not found")
+            raise Exception("Not logged in")
+        params = {
+            "contractId": contract_id
+        }
+        result = await self.__api_request(GAS_CONSUMPTION_URL, params=params)
+        gas = result.get('gas_graph')
+        if gas:
+            return gas
         return None
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="g9_api_client",
-    version="0.0.2",
+    version="0.0.3",
     python_requires='>=3',
     package_dir={'': '.'},
     packages=find_namespace_packages(where='.'),
     install_requires=[
-        "httpx>=0.24.0",
+        "aiohttp>=3.8.5",
     ]
 )
